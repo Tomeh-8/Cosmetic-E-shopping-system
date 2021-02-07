857dd5fb515d82e4327acafa810c431110240edd
Tomeh-8 <tomeh888@gmail.com>
2021-02-07 16:28:57 +0100

    initial commit

---
+++ cosmetic hub (e-shopping system)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cosmetic_hub__e_shopping_system_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
